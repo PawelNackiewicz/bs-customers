9361ab2cc8834fe582c5c990540fe15a11ce7bd7
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2022-12-03 10:14:41 +0100

    init

---
+++ app/products/addProduct/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+      <div>
+        <h1>Hello Add Product</h1>
+      </div>
+    )
+  }
+  
---
+++ app/products/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello Products</h1>
+    </div>
+  )
+}
