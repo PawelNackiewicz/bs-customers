5b8735b1be855235cb18bbb02bf19e86e2438910
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2023-04-17 16:45:46 +0200

    add storybook to icons

---
+++ components/icons/Icon.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { defaultIcons } from './defaultIcons';
+
+import Icon from './Icon';
+
+const meta: Meta<typeof Icon> = {
+  title: 'Icon',
+  component: Icon,
+};
+
+export default meta;
+type Story = StoryObj<typeof Icon>;
+
+export const Icons: Story = {
+  render: () =>
+  (
+    <div>
+      {Object.keys(defaultIcons).map((item) =>
+        <div key={item} style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <Icon name={item} />
+          <p>{item}</p>
+        </div>
+      )}
+    </div>
+  )
+};
